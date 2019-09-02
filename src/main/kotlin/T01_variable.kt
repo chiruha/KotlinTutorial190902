759816b2e01cbb449d03d16a1ecaf2cc48e792a8
YJ <chiruha@naver.com>
2019-09-02 10:49:28 +0900

    first commit

---
+++ src/main/kotlin/T01_variable.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val data1 = "hello world"
+    var data2: Int = 100
+
+//    data1 = 200
+    data2 = 300
+
+}
---
+++ src/main/kotlin/T02_fun.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val result = sum(10,20)
+    println(result)
+
+    val result2 = sum2(100,200)
+    println(result2)
+}
+
+fun sum(a:Int, b:Int=1000):Int {
+    var sum = 0
+    fun calSum() {
+        sum = a+b
+    }
+    calSum()
+    return sum
+}
+
+fun sum2(a:Int, b:Int) = a + b
