ade58a029f949eac00b5da18fa33371eb188ae97
YJ <chiruha@naver.com>
2019-09-02 12:07:26 +0900

    테스트 커밋4

---
+++ src/main/kotlin/T07_Iterator.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val list1 = listOf<String>("hello", "world", "kotlin", "android")
+    val iterator = list1.iterator()
+    while(iterator.hasNext()) {// 처음 호출해야 그 다음을 가르킴
+        println("값 : ${iterator.next()}")
+    }
+
+    val myMap = mapOf<String, String>(
+        "one" to "hello", "two" to "world"
+    )
+
+    val iteratorMap = myMap.iterator()
+    while(iteratorMap.hasNext()) {
+        val res = iteratorMap.next()
+        println("${res.key} :: ${res}")
+
+    }
+}
